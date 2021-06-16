75ee6d455d9a8d4a9cd27e78bcb7db15631ecd63
ekeshwar <81827078+ekeshwar@users.noreply.github.com>
2021-06-17 00:08:04 +0530

    Update IBM_capstone_1.py

--- IBM_capstone_1.py
+++ IBM_capstone_1.py
@@ -1,16 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #this notebook will be mainly used for the capstone project.
 import pandas as pd
 import numpy as np
-
-
-# In[2]:
-
-
 print("Hello Capstone Project Course")
 
